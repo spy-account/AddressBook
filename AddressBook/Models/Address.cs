f6ccac2ab7048347d2ecdde4f5d65bc27c4e519a
cryptic-peanut <bartekddd@op.pl>
2021-03-13 15:19:13 +0100

    Update

---
+++ AddressBook/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Address
+    {
+        [Display(Name = "Ulica")]
+        [StringLength(85, MinimumLength = 4), Required(ErrorMessage = "Pole jest obowiązkowe.")]
+        public string Street { get; set; }
+        [Display(Name = "Kod pocztowy")]
+        [StringLength(6, MinimumLength = 6), Required(ErrorMessage = "Pole jest obowiązkowe.")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Miasto")]
+        [StringLength(85), Required(ErrorMessage = "Pole jest obowiązkowe.")]
+        public string City { get; set; }
+        [Display(Name = "Numer")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe.")]
+        public int Number { get; set; }
+    }
+}
---
+++ AddressBook/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using AddressBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public Address Address { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                Name = "User";
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+            return RedirectToPage("./Privacy");
+        }
+    }
+}
